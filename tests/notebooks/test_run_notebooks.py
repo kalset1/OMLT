903ced45177af929c11f208e9a8cbff61f2ec3b9
kalset1 <kalset3@gmail.com>
2023-06-15 15:14:24 -0400

    fixed method of running notebooks with testbook

--- tests/notebooks/test_run_notebooks.py
+++ tests/notebooks/test_run_notebooks.py
@@ -8,27 +8,34 @@ from omlt.dependencies import keras_available, onnx_available
 
 
 # TODO: These will be replaced with stronger tests using testbook soon
-def _test_run_notebook(folder, notebook_fname, n_cells):
-    # change to notebook directory to allow testing
-    cwd = os.getcwd()
-    os.chdir(os.path.join(this_file_dir(), "..", "..", "docs", "notebooks", folder))
-    with testbook(notebook_fname, timeout=300, execute=True) as tb:
-        assert tb.code_cells_executed == n_cells
-    os.chdir(cwd)
 
+def openBook(folder, notebook_fname):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(this_file_dir(), '..', '..', 'docs', 'notebooks', folder))
+    book = testbook(notebook_fname, execute=True, timeout=300)
+    return book
+
+def run_notebook(tb, n_cells):
+    assert tb.code_cells_executed == n_cells
 
 @pytest.mark.skipif(not keras_available, reason="keras needed for this notebook")
 def test_autothermal_relu_notebook():
-    _test_run_notebook("neuralnet", "auto-thermal-reformer-relu.ipynb", 13)
+    book = openBook('neuralnet', "auto-thermal-reformer-relu.ipynb")
+    with book as tb:
+        run_notebook(tb, 13)
 
 
 @pytest.mark.skipif(not keras_available, reason="keras needed for this notebook")
 def test_autothermal_reformer():
-    _test_run_notebook("neuralnet", "auto-thermal-reformer.ipynb", 13)
+    book = openBook('neuralnet', "auto-thermal-reformer.ipynb")
+    with book as tb:
+        run_notebook(tb, 13)
 
 
 def test_build_network():
-    _test_run_notebook("neuralnet", "build_network.ipynb", 37)
+    book = openBook('neuralnet', "build_network.ipynb")
+    with book as tb:
+        run_notebook(tb, 37)
 
 
 @pytest.mark.skipif(
@@ -36,19 +43,34 @@ def test_build_network():
     reason="onnx and keras needed for this notebook",
 )
 def test_import_network():
-    _test_run_notebook("neuralnet", "import_network.ipynb", 16)
+    book = openBook('neuralnet', "import_network.ipynb")
+    with book as tb:
+        run_notebook(tb, 16)
 
 
 @pytest.mark.skipif(not onnx_available, reason="onnx needed for this notebook")
 def test_mnist_example_convolutional():
-    _test_run_notebook("neuralnet", "mnist_example_convolutional.ipynb", 13)
+    book = openBook('neuralnet', "mnist_example_convolutional.ipynb")
+    with book as tb:
+        run_notebook(tb, 13)
 
 
 @pytest.mark.skipif(not onnx_available, reason="onnx needed for this notebook")
 def test_mnist_example_dense():
-    _test_run_notebook("neuralnet", "mnist_example_dense.ipynb", 13)
+    book = openBook('neuralnet', "mnist_example_dense.ipynb")
+    with book as tb:
+        run_notebook(tb, 13)
 
 
 @pytest.mark.skipif(not keras_available, reason="keras needed for this notebook")
 def test_neural_network_formulations():
-    _test_run_notebook("neuralnet", "neural_network_formulations.ipynb", 21)
+    book = openBook('neuralnet', "neural_network_formulations.ipynb")
+    with book as tb:
+        run_notebook(tb, 21)
+
+@pytest.mark.skipif(not onnx_available, reason='onnx needed for this notebook')
+def test_bo_with_trees():
+    book = openBook('', "bo_with_trees.ipynb")
+    with book as tb:
+        run_notebook(tb, 10)
+
